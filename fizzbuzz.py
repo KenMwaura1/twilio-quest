4d07e87c3eb3083097d3f421e877a0d038491f97
Jimmie Smith <84278458+FriendOfDorothy@users.noreply.github.com>
2021-12-17 10:58:20 -0500

    original fizzbuzz function was timing out when trying to solve. re-written

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,30 +1,12 @@
-# The function accepts INTEGER n as parameter.
 import sys
 
-yx = sys.argv
-yx.pop(0)
-
-
-#
-
-def fizzBuzz(yx):
-    for i in yx:
-        print(i)
-        n = int(i) 
-        for x in range(1,n+1):
-            if x % 3 == 0 and x % 5 == 0:
-                print("FizzBuzz")
-            elif x % 3 == 0:
-                print("Fizz")
-            elif x % 5 == 0:
-                print("Buzz")
-            else:
-                print(x)
-
-
-if __name__ == '__main__':
-    yx = [input().strip()]
-
-    fizzBuzz(yx)
-
-    print(yx)
+inputs = [int(x) for x in sys.argv[1:]]
+for x in inputs:
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+    elif x % 3 == 0:
+        print("fizz")
+    elif x % 5 == 0:
+        print("buzz")
+    else:
+        print(x)
